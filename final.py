986fd7a07a9c79d8d181c351102162a3621e2a39
Max <zimmermannnmax@gmail.com>
2025-06-24 17:24:35 +0200

    Create final.py

---
+++ final.py
@@ -0,0 +1,111 @@
+# %%
+import pandas as pd
+from pandas import read_csv
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy
+
+
+# %%
+df = read_csv("iris.csv", index_col=0)
+
+# %%
+df
+
+# %% [markdown]
+# # Explorative Datenanalyse
+
+# %% 
+df.describe()
+
+# %%
+plt.figure(figsize=(14, 6))
+
+plt.subplot(1, 2, 1)
+sns.histplot(df['sepal_length_cm'], kde=True)
+plt.title('Verteilung der Sepal-Länge')
+
+plt.subplot(1, 2, 2)
+sns.boxplot(y=df['sepal_length_cm'])
+plt.title('Boxplot der Sepal-Länge')
+
+plt.show()
+# %% [markdown]
+# # Interpretation von 2 
+# Die Hälfte der Sepal-Länge (aus beiden Diagrammen ablesbar) befindet sich zwischen ca. 5,2 und 6,5, der Median liegt bei ca. 5,8.\n",
+# Der kleinste Wert liegt bei ca. 4,3 und der größte Wert bei ca. 7,9.
+# Die wenigsten Blumen haben eine Länge von 7,1-7,5, wie man aus dem linken Diagramm ablesen kann.
+# %%
+print("Varianz:", df['sepal_length_cm'].var())
+print("Standardabweichung:", df['sepal_length_cm'].std())
+
+# %%
+sns.scatterplot(data=df, x='sepal_length_cm', y='petal_length_cm', hue='species')
+plt.title('Zusammenhang zwischen Sepal- und Petal-Länge')
+plt.show()
+# %% [markdown]
+# # Interpretation von 3.
+# Setosas haben eine kleine Sepal- als auch eine kleine Petal-Länge.
+# Virginicas haben eine lange Sepal- als auch eine lange Petal-Länge. Es gibt jedoch einen Ausreißer, bei dem die Sepal-Länge kürzer ist als bei den anderen Virginicas.
+# Die Versicolors liegen zwischen den Setosas und Verginicas, grenzen jedoch mehr an die Virginicas als an die Setosas.
+# %%
+corr = df.drop(columns=['species']).corr(numeric_only=True)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+plt.title('Korrelationsmatrix')
+plt.show()
+# %% [markdown]
+## Interpretation von 4.
+# Korrelation:
+# r = +1: perfekte positive Korrelation
+# r = -1: perfekt negative Korrelation
+# r = 0: kein linearer Zusammenhang
+# Sepal width und Sepal Length haben nichts mit einander zu tun. 
+# Hingegen korrelieren Petal length und Petal width stark.
+# %%
+sns.pairplot(df, hue='species')
+plt.show()
+# %%
+
+# %% [markdown]
+# # Unsupervised Learning
+
+# %%
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from scipy.cluster import hierarchy
+
+# %%
+num_data = df.select_dtypes(include= "number") # numerische Daten selektieren
+
+# %%
+Z = hierarchy.linkage(num_data)  
+hierarchy.dendrogram(Z) # Dendogram erstellen
+
+# %%
+# KMeans
+kmeans = KMeans(n_clusters=3, random_state=0)
+df['cluster_kmeans'] = kmeans.fit_predict(num_data)
+
+# Agglomerative Clustering
+agg = AgglomerativeClustering(n_clusters=3)
+df['cluster_agg'] = agg.fit_predict(num_data)
+
+# Visualisierung
+sns.scatterplot(data=df, x='sepal_length_cm', y='petal_length_cm', hue='cluster_kmeans', palette='Set2')
+plt.title('KMeans Clustering')
+plt.show()
+
+sns.scatterplot(data=df, x='sepal_length_cm', y='petal_length_cm', hue='cluster_agg', palette='Set1')
+plt.title('Agglomerative Clustering')
+plt.show()
+# %% [markdown]
+# Bei 2 Clustern wäre es underfitted. Bei 4 Cluster wäre es overfitted.
+# Deswegen haben wir uns bewusst für 3 Cluster entschieden.
+# %% [markdown]
+# # Supervised Learning
+
+
+
+
